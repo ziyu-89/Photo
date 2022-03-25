779337b8ba26b2c003cb6fc2f31163cd2bcf8344
ziyu <4a7g0119@stust.edu.tw>
2022-03-25 22:32:15 +0800

    Initial Commit

---
+++ Photo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photo
+//
+//  Created by 黃子瑜 on 2022/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
