24b3d3aafadb34e196944242c27080619cf331e1
ziyu <4a7g0119@stust.edu.tw>
2022-03-30 16:00:44 +0800

    photo

--- Photo/ViewController.swift
+++ Photo/ViewController.swift
@@ -1,19 +1,83 @@
-//
-//  ViewController.swift
-//  Photo
-//
-//  Created by 黃子瑜 on 2022/3/25.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var catImage: UIImageView!
+    @IBOutlet weak var catPageControl: UIPageControl!
+    @IBOutlet weak var catSegmentedControl: UISegmentedControl!
+   
+    
+    let cat_Image = ["cat1","cat2","cat3"]
+    var number : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        ChangImage()
     }
 
+    //Swipe變更畫面
+    @IBAction func changePage(_ sender: UISwipeGestureRecognizer) {
+        if sender.direction == .left {
+            number += 1
+            if number < 3{
+                ChangImage()}
+            else{
+                number = 0
+                ChangImage()
+            }
+        }
+        else if sender.direction == .right {
+            number -= 1
+            if number == -1 {
+                number = 2
+                ChangImage()}
+            else{
+               
+                ChangImage()
+            }
+        }
+    }
+    //segmentedControl
+    @IBAction func segmentedControl(_ sender: UISegmentedControl) {
+        number = sender.selectedSegmentIndex
+        ChangImage()
+    }
+    
+    //pageControl
+    @IBAction func pageControl(_ sender: UIPageControl) {
+        number = sender.currentPage
+        ChangImage()
+    }
+    
+    //上一頁
+    @IBAction func previous(_ sender: Any) {
+        number -= 1
+        if number == -1 {
+            number = 2
+            ChangImage()
+        }else{
+            ChangImage()
+        }
+    }
+    
+    //下一頁
+    @IBAction func next(_ sender: Any) {
+        number += 1
+        if number < 3 {
+            ChangImage()
+        }else{
+            number = 0
+            ChangImage()
+        }
+        
+    }
+    
+    //同時改變的內容
+    func ChangImage(){
+        catImage.image = UIImage(named: cat_Image[number])
+        catPageControl.currentPage = number
+        catSegmentedControl.selectedSegmentIndex = number
+    }
 
 }
 
